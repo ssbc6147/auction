3f91b94ebe1e5dcbc22a028bf81ab6f1ec067c77
ssbc6147 <6147mgcn@gmail.com>
2019-11-12 15:52:50 +0900

    更新処理作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,15 @@ class ItemsController < ApplicationController
         redirect_to @item
     end
 
+    def edit
+        @item = Item.find(params[:id])
+    end
+    def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        redirect_to @item
+    end
+
     private
 
     def item_params
